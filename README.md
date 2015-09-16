5a305575989f7ab31681500081d9de042ea03951
Xueshan Feng <sfeng@stanford.edu>
2015-09-16 14:04:28 -0700

    New version.

---
+++ README.md
@@ -0,0 +1,143 @@
+
+This document shows an example of how to set up SSO for AWS console access with an 
+IAM role, using Stanford production IdP service as identity provider.  
+
+By using AWS SAML integration, you don't need to create AWS accounts for users who need to access AWS console. Instead, You create a new or use an existing Stanford workgroup that contains users who will use the SSO to gain access to your account's AWS console. 
+
+## Using stanford-sso.sh command line tool
+
+The following instructions apply to MacOS. For other platforms, follow the tool links and instructions on tool sites.
+
+1. Install [Jq](http://stedolan.github.io/jq/)
+
+    ```
+    $ brew install jq
+    ```
+    
+1. Install and configure [AWS CLI](https://github.com/aws/aws-cli)
+
+  If you have AWSCLI installed and configured, you can skip this step.
+
+    ```
+    $ brew install awscli
+    ```
+    or
+
+    ```
+    $ sudo easy_install pip
+    $ sudo pip install --upgrade awscli
+    ```
+    
+    ```
+    $ awscli configure --profile <aws user>
+    ```
+ You will be prompted for AWS KEY and AWS SECRET for the aws user. The profile name will be used for AWS authentication/authorizatio to make AWS CLI calls. 
+
+1. Clone the repo
+
+    ```
+    $ git clone https://github.com/Stanford/AWS-SSO.git
+    $ cd AWS-SSO
+    ```
+    
+1. Run help
+    
+    ```
+    $ ./stanford-sso.sh
+    ```
+
+1. Create SAML provider 
+ 
+ Dry-run:
+ 
+    ```
+    $ ./stanford-sso.sh -a create -c <aws profile> -n stanford-idp -p AdministratorAccess -w myworkgroup -d
+    ```
+
+ The above command will do a dry-run to show what will be created. __stanford-idp__ is a descriptive name to identify the idp provider you use. You can pass in the medtadata url for the idp provider on the command line (see help). The default metadata is 'https://idp.stanford.edu/metadata.xml'
+ 
+ 
+ Real run:
+ 
+     ```
+     $ ./stanford-sso.sh -a create -c <aws profile> -n stanford-idp -p AdministratorAccess -w myworkgroup
+     All done! Next step. Submit the following request to https://helpsu.stanford.edu/helpsu/3.0/auth/helpsu-form?pcat=shibboleth to create idp server setup.
+
+     When idp server setup is complete, you can login to AWS console SSO through this url:
+     https://idp.stanford.edu/idp/profile/SAML2/Unsolicited/SSO?providerId=urn:amazon:webservices
+
+           Account number: 123456789012
+           Provider name: arn:aws:iam::123456789012:saml-provider/stanford-idp
+           Role-name: AdministratorAccess-generatedby-stanford-sso
+           Workgroup: myworkgroup
+    ```
+
+1. Delete SAML provider
+
+    ```
+    $ ./stanford-sso.sh -a delete -c <aws profile> -n stanford-idp -p AdministratorAccess -w myworkgroup 
+    ```
+    
+## Manual steps
+
+### identity provider setup
+
+Login to your AWS console.
+
+1. Select IAM service
+1. Click 'Identity Providers' 
+1. Click 'Create SAML Provider'
+    Choose a name that is easy to identify which provider provides SSO, e.g. stanford-idp. 
+1. Upload idP-only SAML metadata document from:
+
+       [idp-only metadata](https://idp.stanford.edu/metadata.xml)
+   
+    Click "Create" button to finish the provider setup.
+
+### Create IAM role using this provider in the trust policy
+
+   When you finish identy provider creation, there is a link to take you
+   to create an IAM role, or you you can go back to IAM service, select 
+   "Roles->Create New Role".
+
+1. Create role name: e.g. myadminaccount-sso
+1. Select __Role for Identity Provider Access__ in the role type selection screen near
+   the bottom.
+1. Select __Grant Web Single Sign-On (WebSSO) access to SAML providers__
+1. Accept the default "Verify Role Trust" policy.
+1. Set permissions: assign the role a permission, e.g. Administrator, Power admin user, etc. It depends on your use case.  
+1. Click "Create Role" to finish
+
+##  Configure relying party trust between  IdP and AWS
+
+Amazon currently only works with IdP-initiated SSO - our Unsolicited SSO Endpoints 
+are not listed in our IdP metadata, but the handlers and decoders appear to be 
+enabled.  AWS only uses the metadata for the certificate / entity ID.
+
+You need to submit the request to [HelpSU](https://helpsu.stanford.edu/helpsu/3.0/auth/helpsu-form?pcat=shibboleth) to complete the setup with the following information.
+
+* The [AWS metadata](https://signin.aws.amazon.com/static/saml-metadata.xml)
+
+* Attribute Resolver Settings
+
+  Replace the account number, role-name, and workgroup with the values you created in the SSO setup steps. You can find the information from AWS console under IAM->Roles, IAM->Identity Provider sections. 
+  
+        Account number: 123456789012
+        Provider name: arn:aws:iam::123456789012:saml-provider/stanford-idp
+        Role-name: myadminaccount-sso
+        Workgroup: awsworkgroup
+
+##  Create a virtual host to access the your accoount's AWS console
+
+After idp service is updated with your AWS SSO data, you can access AWS console by going to:
+
+        https://idp.stanford.edu/idp/profile/SAML2/Unsolicited/SSO?providerId=urn:amazon:webservices
+
+Note that if you have multiple accounts with the same idp provider, you will see the list of SSO accounts on AWS console. Pick the account number for which you want to login.
+
+You can also create a virtual hostname, e.g.  "my-account-aws-console.stanford.edu"
+and redirect it to:
+
+        https://idp.stanford.edu/idp/profile/SAML2/Unsolicited/SSO?providerId=urn:amazon:webservices
+        
+
---
+++ stanford-sso.sh
@@ -0,0 +1,261 @@
+#!/bin/bash
+#  Copyright 2015
+#  The Board of Trustees of the Leland Stanford Junior University
+
+set -e
+shopt -s extglob # enables pattern lists like +(...|...)
+# Default variables
+init() {
+  # Script name
+  script=$(basename "${BASH_SOURCE[0]}" .sh)
+
+  # Script actions
+  actionsRegexp='+(create|delete|show)'
+  
+  # Dryrun flag
+  dryrun=0
+  
+  # Timestamp
+  date=$(date +%Y%m%d)
+
+  # Interactive flag
+  interactive=1  
+
+  # Metadata URL
+  metadataUrl='https://idp.stanford.edu/metadata.xml'
+}
+
+# Create saml provider
+create_saml_provider() {
+  samlMetadata=$(curl -s -k $metadataUrl | tee /tmp/samlMetadata.xml)
+  if ! [[ "$samlMetadata" =~ "entityID=\"https://" ]];
+  then
+    echo "$samlMetadata"
+    echo "Invalid metadata"
+    exit 1
+  fi
+
+  echo "Creating saml provider $name."
+  cmd="aws --profile $profile iam create-saml-provider --name=$name --output=text --saml-metadata-document file:///tmp/samlMetadata.xml"
+  [ $dryrun -eq 0 ] && $cmd || echo $cmd 
+
+  # If no account alias, create one
+  accountAlias=$(aws --profile itlab  iam list-account-aliases | jq --raw-output '.AccountAliases[]')
+  if [ -z "$accountAlias" ]; 
+  then
+    echo "Creating account alias ${script}-${profile}"
+    cmd="aws --profile $profile iam create-account-alias --account-alias ${script}-${profile}"
+    [ $dryrun -eq 0 ] && $cmd || echo $cmd 
+  fi
+}
+
+# Show SAML provider
+show_saml_provider() {
+  aws --profile $profile iam get-saml-provider --saml-provider-arn=$samlProviderArn
+}
+
+# Delete SAML provider
+delete_saml_provider() {
+  echo "Deleting saml provider $name."
+  cmd="aws --profile $profile iam delete-saml-provider --saml-provider-arn=$samlProviderArn"
+  [ $dryrun -eq 0 ] && $cmd || echo $cmd 
+ 
+  # Also remove alias we created
+  accountAlias=$(aws --profile itlab  iam list-account-aliases | jq --raw-output '.AccountAliases[]')
+  if [ "$accountAlias" = "${script}-${profile}" ];
+  then
+    cmd="aws --profile $profile iam delete-account-alias --account-alias ${script}-${profile}"
+    [ $dryrun -eq 0 ] && $cmd || echo $cmd 
+  fi
+}
+
+# Create role
+create_role() {
+  cat > /tmp/trust-policy.json <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Federated": "arn:aws:iam::$accountId:saml-provider/$name"
+      },
+      "Action": "sts:AssumeRoleWithSAML",
+      "Condition": {
+        "StringEquals": {
+          "SAML:aud": "https://signin.aws.amazon.com/saml"
+        }
+      }
+    }
+  ]
+}
+EOF
+
+  # Create role and assume idp trust policy
+  echo "Creating role $roleName"
+  cmd1="aws --profile $profile iam create-role --role-name $roleName --assume-role-policy-document file:///tmp/trust-policy.json"
+  # Grant access 
+  cmd2="aws --profile $profile iam attach-role-policy --role-name $roleName --policy-arn $policyArn"
+  [ $dryrun -eq 0 ] && $cmd1 && $cmd2 || echo $cmd1 && echo $cmd2 
+}
+  
+# Delete role - only delete role created today with this script. 
+delete_role() {
+  echo "Deleting role $roleName"
+  cmd1="aws --profile $profile iam detach-role-policy --role-name $roleName --policy-arn=$policyArn"
+  cmd2="aws --profile $profile iam delete-role --role-name $roleName"
+  [ $dryrun -eq 0 ] && $cmd1 && $cmd2 || echo $cmd1 && echo $cmd2
+}
+
+# Output
+print_info() {
+  if [ $action = 'create' ]; then
+    echo ""
+    echo "All done! Next step. Submit the following request to https://helpsu.stanford.edu/helpsu/3.0/auth/helpsu-form?pcat=shibboleth to create idp server setup."
+    echo ""
+    echo "When idp server setup is complete, you can login to AWS console SSO through this url:"
+    echo "https://idp.stanford.edu/idp/profile/SAML2/Unsolicited/SSO?providerId=urn:amazon:webservices"
+    echo ""
+  elif [ $action = 'delete' ]; then
+    echo "All done! Next step. Submit the following request to https://helpsu.stanford.edu/helpsu/3.0/auth/helpsu-form?pcat=shibboleth to remove idp server setup."
+  fi
+  cat <<EOF
+      Account number: $accountId
+      Provider name: arn:aws:iam::$accountId:saml-provider/$name
+      Role-name: $roleName
+      Workgroup: $workgroup
+EOF
+}
+
+# Docs
+help(){
+  echo "stanford-sso -a <action> -c <config> [-n <name>] -p <permissino> [-d]"
+  echo ""
+  echo " -a <create|show|delete>: action. create, show or delete SSO setup by this tool."
+  echo " -c <aws config>: authenticate using profile defined by configuration."
+  echo " -n <provider-name>: the name of the idp provider, for example 'stanford-idp'."
+  echo " -u <url-for-metadata>: optional. idp provider metadata url. Default 'https://idp.stanford.edu/metadata.xml'."
+  echo " -p <ReadOnlyAccess|AdministratorAccess|list-policies>: ReadOnlyAccess, AdministratorAccess, or list other valid AWS managed polices."
+  echo " -w <workgroupname>: Stanford workgroup name to link into this saml provider setup. e.g. itlab:anchorage-admin" 
+  echo " -d     : dryrun. print out the commands"
+  echo " -h     : Help"
+}
+
+# Main
+
+# Set default values
+init
+
+while getopts "a:c:p:n:u:w:hd" OPTION
+do
+  case $OPTION in
+    a)
+      action=$OPTARG
+      case $action in
+        $actionsRegexp) 
+           ;;
+        *)
+          echo "Unsupported action $action."
+          exit 1
+      esac
+      ;;
+    c)
+      profile=$OPTARG
+      ;;
+    p)
+      permission=$OPTARG
+      ;;
+    n)
+      name=$OPTARG
+      ;;
+    u)
+      metadataUrl=$OPTARG
+      if [ "$metadata" !~ "https" ]; 
+      then
+        echo "metadata url should contain https."
+        echo "e.g. https://idp.stanford.edu/metadata.xml"
+        exit 1
+      fi
+      ;;
+    w)
+      workgroup=$OPTARG
+      ;;
+    d)
+      dryrun=1
+      ;;
+    [h?])
+      help
+      exit
+      ;;
+  esac
+done
+
+if [[ -z $action || -z $profile || -z $name ]]; then
+  help
+  echo "-a, -c, and -n  are required."
+  exit 1
+elif [[ $action =~ ^(create|delete)$ && -z $permission && -z $workgroup ]]; then
+  echo "create or delete requires access permission and workgroup name."
+  exit 1
+fi
+
+echo "Getting AWS account number ..."
+accountId=$(aws --profile $profile iam get-user | jq '.User.Arn' | grep -Eo '[[:digit:]]{12}')
+if [ -z "$accountId" ]; then
+  echo "Cannot find AWS account number."
+  exit 1
+fi
+
+# Valid managed policy name
+policyArn=$(aws --profile $profile iam list-policies | jq --raw-output --arg p "$permission" '.Policies[] | select(.PolicyName == $p).Arn')
+if [ -z "$policyArn" ];
+then
+  aws --profile $profile iam list-policies | jq --arg p "$permission" --raw-output '.Policies[].PolicyName' | sort 
+  echo "Valid policies are shown as above."
+  exit 1
+else
+  roleName=${permission}-generatedby-${script}
+fi
+
+# Get saml provider arn
+echo "$action $name"
+samlProviderArn=$(aws --profile $profile iam list-saml-providers | jq --raw-output ".SAMLProviderList[] | select(.Arn == \"arn:aws:iam::$accountId:saml-provider/$name\") | .Arn")
+
+# Call functions based on action 
+case $action in
+  'create')
+    if  [ "$samlProviderArn" == "arn:aws:iam::$accountId:saml-provider/$name" ];
+    then
+     show_saml_provider
+     echo "SAML provider $name is already setup."
+     exit 0
+    else
+      create_saml_provider
+      create_role
+    fi
+    ;;
+  'show')
+    if  [ "$samlProviderArn" != "arn:aws:iam::$accountId:saml-provider/$name" ];
+    then
+      echo "SAML provider $name doesn't exist."
+      exit 1
+    else
+      show_saml_provider
+    fi
+    ;; 
+  'delete')
+    if  [ "$samlProviderArn" != "arn:aws:iam::$accountId:saml-provider/$name" ];
+    then
+      echo "SAML provider $name doesn't exist."
+      exit 1
+    else
+      delete_saml_provider
+      delete_role
+    fi
+    ;; 
+esac
+
+[ $dryrun -eq 1 ] && echo "Dryrun mode. Nothing is changed." || print_info 
+
+exit 0
